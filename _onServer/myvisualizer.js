5491ffa85b9b34548acc4365323dfa78f1be66c5
張書懷 <junsuwhy@yahoo.com.tw>
2013-08-30 16:34:01 +0800

    換資料類型時不會把時間區間改回1小時
    就是選兩小時之後, 再換資料類型成點擊次數時, 會看到是更改現有長條的長度, 而不是重畫一個.
    但是如果換小時/星期的部份, 就會重畫了

--- _onServer/myvisualizer.js
+++ _onServer/myvisualizer.js
@@ -1,3 +1,9 @@
+$dataType={
+	durat:'h',
+	type1:'cmeo',
+	timeInterval:1
+}
+
 var $D3currArray=null;
 var $svg=null;
 var $g1=null;
@@ -6,9 +12,71 @@ var $currArray=null;
 
 var $x;
 var $y;
+var $height;
+
+
+
+	//設定一些變數
+	//寬
+	width=800;
+	//高
+	height=480;
+	//留白
+	padding=70;
+	//每條長條旁邊的留白寬度跟長條寬度的比例
+	filler="2:1";
+	arrFil=[];
+	arrFil[0]=parseInt(filler[0]);
+	arrFil[1]=parseInt(filler[2]);
+	//單一一個文字的像素
+	em=16;
+	//後來沒什麼用到的變數
+	xlength=width-2*padding;
+	ylength=height-2*padding;
+
+	//長條旁邊的留白跟長條加總的寬度
+	unitWidth=function(arr){return xlength/arr.length};
+	//長條的寬度, 用上面那個數字乘上比例filler
+	rectWidth=function(arr){return arrFil[1]*unitWidth(arr)/(arrFil[0]+arrFil[1])};
+	//長條之間的留白寬度
+	offsetWidth=function(arr){return arrFil[0]*unitWidth(arr)/(arrFil[0]+arrFil[1])};
+
+	//取得最大值, 後來發現d3.max好像就可以用了
+	arrMax=function(){
+		max=$currArray[0]
+		for (var i = 0; i < $currArray.length; i++) {
+			if($currArray[i]>max)max=$currArray[i];
+		};
+		return max;
+	}
+	
+
+		//取得cmet/cmeo的比例
+	//horw代入"h"或"w"可取得hour 或weekday的值
+	arrCmetOverCmeo=function(horw){
+		arrCmet=arrFromObj($jsonData["cmet."+horw]);
+		arrCmeo=arrFromObj($jsonData["cmeo."+horw]);
+		arr=new Array();
+		for (var i = 0; i < arrCmet.length; i++) {
+			arr.push(arrCmet[i]/arrCmeo[i]);
+		};
+
+		return arr;
+	}
+
+	arrFromCSA=function(csaKey,horw){
+		arrCSA=arrFromObj($jsonData['ccb.'+horw]);
+		arrReturn=[];
+		for (var i = 0; i < arrCSA.length; i++) {
+			arrReturn.push(arrCSA[i][csaKey]);
+		};
+		return arrReturn;
+
+	}
 
 //換區間
 showTimeRange=function(intVal){
+	$dataType.timeInterval=intVal;
 	arrYPosition=[];
 	for (var i = 0; i < $currArray.length; i++) {
 		if(i%intVal==0){
@@ -66,7 +134,7 @@ showTimeRange=function(intVal){
 				str+=',';
 				str+=($y(d))-em;
 				str+=')';
-				str+='rotate(-90)';
+				str+='rotate(-75)';
 				return str;
 			})
 			.attr('class','value')
@@ -81,7 +149,7 @@ showTimeRange=function(intVal){
 	}else{
 
 		for (var i = 0; i < arrYPosition.length/intVal; i++) {
-			$svg.append('text').attr('class','value')
+			$svg.append('text').attr('class','tag')
 				.text((i*intVal+1)+'到'+(i+1)*intVal+"點")
 				.attr('x',$x(i*intVal)-(3)*rectWidth($currArray))
 				.attr('y',height-padding+em)
@@ -101,7 +169,7 @@ showTimeRange=function(intVal){
 					return str;
 				})
 				.attr('class','value')
-				.text(Math.floor((arrYPosition[((i+1)*intVal)-1]*1000)/1000))
+				.text(Math.floor(arrYPosition[((i+1)*intVal)-1]*1000)/1000)
 				.style('fill-opacity',0)
 				.transition().duration(1500)
 				.style('fill-opacity',1);
@@ -130,14 +198,16 @@ showsortit=function(bVal){
 			return $x(orderArray[i])-rectWidth($currArray);
 		});
 
-		$svg.selectAll('.tag').transition().duration(1500).delay(function(d,i){
+		$svg.selectAll('.tag').transition()
+			.duration(1500).delay(function(d,i){
 			return i*30
 		})
 		.attr('x',function(d,i){
 			return $x(orderArray[i])-(1.5)*rectWidth($currArray);
 		});
 
-		$svg.selectAll('.value').transition().duration(1500).delay(function(d,i){
+		$svg.selectAll('.value').transition()
+			.duration(1500).delay(function(d,i){
 			return i*30
 		})
 		.attr('transform',function(d,i){
@@ -147,7 +217,7 @@ showsortit=function(bVal){
 			str+=',';
 			str+=($y(d))-em;
 			str+=')';
-			str+='rotate(-90)';
+			str+='rotate(-75)';
 			return str;
 		});
 	
@@ -155,163 +225,158 @@ showsortit=function(bVal){
 
 };
 
+//建立新的svg繪圖物件, 用在初始時.換小時/星期的情況
+addSvgObjects=function(jsonData,dataType){
+	$dataType.timeInterval=1;
+	$jsonData=jsonData;
+	$dataType.type1=dataType.split('.')[0];
+	$dataType.durat=dataType.split('.')[1];
+
+	//初始化 清空#svg-container, 設定$svg,$g1
+	$("#svg-container").empty();
+	$("#svg-container").append('<svg>');
+	$svg=d3.select('#svg-container svg');
+	$svg.attr('width',width).attr('height',height);
+	$g1=$svg.append("g").attr('id','group1').selectAll('#group1');
+
+
+
+
+		//指定要代入的資料矩陣是什麼
+	if($dataType.type1=='cmeto'){
+		//cmeto是cmet/cmeo的值
+		$currArray=arrCmetOverCmeo($dataType.durat);
+	}else if($dataType.type1.substr(0,3)=='ccb'){
+		//此時$dataType.type1整個字串為ccb->count 這種型式, 要再拆開取出->後面的字串
+		csaKey=$dataType.type1.split('->')[1];
+		$currArray=arrFromCSA(csaKey,$dataType.durat);
+	}else{
+		//剩下cmeo, cmet
+		$currArray=arrFromObj($jsonData[dataType]);
+	}
+
+		//給定D3currArray  是d3.data.enter的物件
+	$D3currArray=$g1.data($currArray).enter();
+
+	//設定$x,$y,$height三個 domain 函數
+	$x=d3.scale.ordinal()
+		.domain(d3.range($currArray.length))
+		.rangePoints([padding,width-padding]);
+	$y=d3.scale.linear()
+		.domain([0,arrMax()])
+		.range([height-padding,padding]);
+	$height=d3.scale.linear()
+		.domain([0,arrMax()])
+		.range([0,height-2*padding]);
+
+
+
+	//畫長條
+	$D3currArray.append('rect')
+		.attr('x',function(d,i){
+			return $x(i)-rectWidth($currArray);
+		})
+		.attr('y',function(d,i){
+			return height-padding;
+		})
+		.attr('width',function(d,i){
+			return rectWidth($currArray)
+		})
+		.attr("height",0);
+
+		//加入tag的字
+	$D3currArray.append('text').text(function(d,i){return i})
+		.attr('x',function(d,i){
+			return $x(i)-(1.5)*rectWidth($currArray);
+		})
+		.attr('y',function(d,i){
+			return height-padding+em;
+		})
+		.attr('class','tag')
+		.style('fill-opacity',1);
+
+	//加入value的字
+	$D3currArray.append('text')
+		.attr('class','value');
+
+
+}
+
+//
 visualize=function(jsonData,dataType){
 
 	$jsonData=jsonData;
-	type1=dataType.split('.')[0];
-	typeDura=dataType.split('.')[1];
-
-	//取得cmet/cmeo的比例
-	//horw代入"h"或"w"可取得hour 或weekday的值
-	arrCmetOverCmeo=function(horw){
-		arrCmet=arrFromObj($jsonData["cmet."+horw]);
-		arrCmeo=arrFromObj($jsonData["cmeo."+horw]);
-		arr=new Array();
-		for (var i = 0; i < arrCmet.length; i++) {
-			arr.push(arrCmet[i]/arrCmeo[i]);
-		};
-
-		return arr;
-	}
-
-	arrFromCSA=function(csaKey,horw){
-		arrCSA=arrFromObj($jsonData['ccb.'+horw]);
-		arrReturn=[];
-		for (var i = 0; i < arrCSA.length; i++) {
-			arrReturn.push(arrCSA[i][csaKey]);
-		};
-		return arrReturn;
-
-	}
-
-	//設定全域變數
-	width=800;
-	height=480;
-	padding=70;
-	filler="2:1";
-	arrFil=[];
-	arrFil[0]=parseInt(filler[0]);
-	arrFil[1]=parseInt(filler[2]);
-	em=16;
-
-	center={
-		x:padding,
-		y:height-padding
-	}
-	xlength=width-2*padding;
-	ylength=height-2*padding;
-
-	unitWidth=function(arr){return xlength/arr.length};
-	rectWidth=function(arr){return arrFil[1]*unitWidth(arr)/(arrFil[0]+arrFil[1])};
-	offsetWidth=function(arr){return arrFil[0]*unitWidth(arr)/(arrFil[0]+arrFil[1])};
-
-	//取得最大值
-	arrMax=function(){
-		max=$currArray[0]
-		for (var i = 0; i < $currArray.length; i++) {
-			if($currArray[i]>max)max=$currArray[i];
-		};
-		return max;
-	}
+	$dataType.type1=dataType.split('.')[0];
+	$dataType.durat=dataType.split('.')[1];
 
 
-	main=function(){
 
-		//初使化
-		$("#svg-container").empty();
-		$("#svg-container").append('<svg>');
-		$svg=d3.select('#svg-container svg');
-		$svg.attr('width',width).attr('height',height);
-		$g1=$svg.append("g").attr('id','group1').selectAll('#group1');
+
 
 		//指定要代入的資料矩陣是什麼
-		if(type1=='cmeto'){
-			$currArray=arrCmetOverCmeo(typeDura);
-		}else if(type1.substr(0,3)=='ccb'){
-			csaKey=type1.split('->')[1];
-			$currArray=arrFromCSA(csaKey,typeDura);
-		}else{
-			$currArray=arrFromObj($jsonData[dataType]);
-		}
+	if($dataType.type1=='cmeto'){
+		//cmeto是cmet/cmeo的值
+		$currArray=arrCmetOverCmeo($dataType.durat);
+	}else if($dataType.type1.substr(0,3)=='ccb'){
+		//此時$dataType.type1整個字串為ccb->count 這種型式, 要再拆開取出->後面的字串
+		csaKey=$dataType.type1.split('->')[1];
+		$currArray=arrFromCSA(csaKey,$dataType.durat);
+	}else{
+		//剩下cmeo, cmet
+		$currArray=arrFromObj($jsonData[dataType]);
+	}
+
+		//給定D3currArray  是d3.data.enter的物件
+	$D3currArray=$g1.data($currArray).enter();
+
+	//設定$x,$y,$height三個 domain 函數
+	$x=d3.scale.ordinal()
+		.domain(d3.range($currArray.length))
+		.rangePoints([padding,width-padding]);
+	$y=d3.scale.linear()
+		.domain([0,arrMax()])
+		.range([height-padding,padding]);
+	$height=d3.scale.linear()
+		.domain([0,arrMax()])
+		.range([0,height-2*padding]);
+
+	
+	//畫長條
+	$svg.selectAll('rect')
+		.property('__data__',function(d,i){
+			return $currArray[i]
+		})
+		.attr('fill','blue');
+		
+
+/*
+	//加入tag的字
+	$D3currArray.append('text').text(function(d,i){return i})
+		.attr('x',function(d,i){
+			return $x(i)-(1.5)*rectWidth($currArray);
+		})
+		.attr('y',function(d,i){
+			return height-padding+em;
+		})
+		.attr('class','tag')
+		.style('fill-opacity',1);
+		*/
+
+	//加入value的字
+	$svg.selectAll('.value')
+		.property('__data__',function(d,i){
+			return $currArray[i]
+		})
+		
+		showTimeRange($dataType.timeInterval);
 
 
-		$x=d3.scale.ordinal()
-			.domain(d3.range($currArray.length))
-			.rangePoints([padding,width-padding]);
-		$y=d3.scale.linear()
-			.domain([0,arrMax()])
-			.range([height-padding,padding]);
-		$height=d3.scale.linear()
-			.domain([0,arrMax()])
-			.range([0,height-2*padding]);
-
-		$D3currArray=$g1.data($currArray).enter();
-
-		$D3currArray.append('rect')
-			.attr('x',function(d,i){
-				return $x(i)-rectWidth($currArray);
-			})
-			.attr('y',function(d,i){
-				return height-padding;
-			})
-			.attr('width',function(d,i){
-				return rectWidth($currArray)
-			})
-			.attr("height",0)
-			.transition()
-			.duration(1500)
-			.attr('y',function(d,i){
-				return $y(d);
-			})
-			.attr('height',function(d,i){
-				return $height(d)
-			})
-			.attr('fill','blue');
-
-		//加入tag的字
-		$D3currArray.append('text').text(function(d,i){return i})
-			.attr('x',function(d,i){
-				return $x(i)-(1.5)*rectWidth($currArray);
-			})
-			.attr('y',function(d,i){
-				return height-padding+em;
-			})
-			.attr('class','tag')
-			.style('fill-opacity',1);
-
-		//加入value的字
-		$D3currArray.append('text')
-			.attr('transform',function(d,i){
-				str="";
-				str+='translate(';
-				str+=($x(i));
-				str+=',';
-				str+=($y(0))-em;
-				str+=')';
-				str+='rotate(-90)';
-				return str;
-			})
-			.transition().duration(1500)
-			.attr('transform',function(d,i){
-				str="";
-				str+='translate(';
-				str+=($x(i));
-				str+=',';
-				str+=($y(d))-em;
-				str+=')';
-				str+='rotate(-90)';
-				return str;
-			})
-			.attr('class','value')
-			.text(function(d,i){
-				return (Math.floor(d*1000)/1000)
-			})
-			.style('fill-opacity',1);
-
-
-	};
-
-	main();
 
 };
-
+//函式:把object轉成array
+arrFromObj=function(myObj){
+  arr=[]
+  $.each(myObj, function(i,n) {
+    arr.push(n);});
+  return arr;
+}
